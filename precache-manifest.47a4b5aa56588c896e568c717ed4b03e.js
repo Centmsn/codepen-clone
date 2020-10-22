89890c9b78d1606199c7adc1e7c6a83ed97df4fd
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-10-22 15:31:53 +0200

    Updates

---
+++ precache-manifest.47a4b5aa56588c896e568c717ed4b03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a8af461486be0835aea94b1f2d1989",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "ddea524daf9624310871",
+    "url": "/codepen-clone/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "62cd05fe5b94dc69baee",
+    "url": "/codepen-clone/static/css/main.d423cef5.chunk.css"
+  },
+  {
+    "revision": "ddea524daf9624310871",
+    "url": "/codepen-clone/static/js/2.acff095e.chunk.js"
+  },
+  {
+    "revision": "d2ff67a5f290a85b508a595a768d7cfc",
+    "url": "/codepen-clone/static/js/2.acff095e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cd05fe5b94dc69baee",
+    "url": "/codepen-clone/static/js/main.ccace64e.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9448bafde3765d042a6d0677a5957fab.js"
+  "/codepen-clone/precache-manifest.47a4b5aa56588c896e568c717ed4b03e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codepen-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
