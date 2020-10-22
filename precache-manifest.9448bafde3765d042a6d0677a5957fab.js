252c2537a6dfa8c8a6610f22a5c99b0900629041
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-10-22 15:29:36 +0200

    Updates

---
+++ precache-manifest.9448bafde3765d042a6d0677a5957fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24dacc0d432e51b2400f9429f0252ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddea524daf9624310871",
+    "url": "/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "62cd05fe5b94dc69baee",
+    "url": "/static/css/main.d423cef5.chunk.css"
+  },
+  {
+    "revision": "ddea524daf9624310871",
+    "url": "/static/js/2.acff095e.chunk.js"
+  },
+  {
+    "revision": "d2ff67a5f290a85b508a595a768d7cfc",
+    "url": "/static/js/2.acff095e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cd05fe5b94dc69baee",
+    "url": "/static/js/main.ccace64e.chunk.js"
+  },
+  {
+    "revision": "44d459044b9da8ae1201",
+    "url": "/static/js/runtime-main.6829a894.js"
+  }
+]);
